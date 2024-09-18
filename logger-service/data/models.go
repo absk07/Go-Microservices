c551f0df1c7c1109731903cfd1cb7cee5c6645aa
absk <noobmaster26128@gmail.com>
2024-09-19 00:30:46 +0530

    updated log-service

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -25,21 +25,29 @@ type Models struct {
 }
 
 type LogEntry struct {
-	ID string `bson:"_id,omitempty" json:"id,omitempty"`
-	Name string `bson:"name" json:"name"`
-	Data string `bson:"data" json:"data"`
-	CreatedAt time.Time `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+	ID           string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Method       string    `bson:"method" json:"method"`
+	Path         string    `bson:"path" json:"path"`
+	RemoteAddr   string    `bson:"remote_addr" json:"remote_addr"`
+	ResponseTime string    `bson:"response_time" json:"response_time"`
+	StartTime    string    `bson:"start_time" json:"start_time"`
+	StatusCode   string    `bson:"status_code" json:"status_code"`
+	CreatedAt    time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt    time.Time `bson:"updated_at" json:"updated_at"`
 }
 
 func (l *LogEntry) Insert(entry LogEntry) error {
 	collection := client.Database("logs").Collection("logs")
 
 	_, err := collection.InsertOne(context.Background(), LogEntry{
-		Name: entry.Name,
-		Data: entry.Data,
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
+		Method:       entry.Method,
+		Path:         entry.Path,
+		RemoteAddr:   entry.RemoteAddr,
+		ResponseTime: entry.ResponseTime,
+		StartTime:    entry.StartTime,
+		StatusCode:   entry.StatusCode,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
 	})
 
 	if err != nil {
@@ -57,7 +65,7 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 	collection := client.Database("logs").Collection("logs")
 
 	opts := options.Find()
-	opts.SetSort(bson.D{{ "created_at", -1 }})
+	opts.SetSort(bson.D{{"created_at", -1}})
 
 	cursor, err := collection.Find(ctx, bson.D{}, opts)
 	if err != nil {
@@ -93,7 +101,7 @@ func (l *LogEntry) GetOne(id string) (*LogEntry, error) {
 	}
 
 	var entry LogEntry
-	err = collection.FindOne(ctx, bson.M{ "_id": _id }).Decode(&entry)
+	err = collection.FindOne(ctx, bson.M{"_id": _id}).Decode(&entry)
 	if err != nil {
 		log.Println("Error finding log by _id", err)
 		return nil, err
@@ -115,35 +123,35 @@ func (l *LogEntry) DropCollection() error {
 	return nil
 }
 
-func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+// func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
 
-	collection := client.Database("logs").Collection("logs")
+// 	collection := client.Database("logs").Collection("logs")
 
-	_id, err := primitive.ObjectIDFromHex(l.ID)
-	if err != nil {
-		log.Println("Error converting _id", err)
-		return nil, err
-	}
+// 	_id, err := primitive.ObjectIDFromHex(l.ID)
+// 	if err != nil {
+// 		log.Println("Error converting _id", err)
+// 		return nil, err
+// 	}
 
-	result, err := collection.UpdateOne(
-		ctx,
-		bson.M{ "_id": _id },
-		bson.D{
-			{ 
-				"$set", bson.D{
-					{ "name", l.Name },
-					{ "data", l.Data },
-					{ "updated_at", time.Now() },
-				},
-			},
-		},
-	)
+// 	result, err := collection.UpdateOne(
+// 		ctx,
+// 		bson.M{ "_id": _id },
+// 		bson.D{
+// 			{
+// 				"$set", bson.D{
+// 					{ "name", l.Name },
+// 					{ "data", l.Data },
+// 					{ "updated_at", time.Now() },
+// 				},
+// 			},
+// 		},
+// 	)
 
-	if err != nil {
-		return nil, err
-	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return result, nil
-}
+// 	return result, nil
+// }
--- logger-service/src/api/handlers.go
+++ logger-service/src/api/handlers.go
@@ -8,36 +8,44 @@ import (
 )
 
 type logRequest struct {
-	Name string `json:"name"`
-	Data string `json:"data"`
+	Method       string `json:"method"`
+	Path         string `json:"path"`
+	RemoteAddr   string `json:"remote_addr"`
+	ResponseTime string `json:"response_time"`
+	StartTime    string `json:"start_time"`
+	StatusCode   string `json:"status_code"`
 }
 
 func (app *Config) WriteLog(ctx *gin.Context) {
 	var req logRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": true,
+			"error":   true,
 			"message": err,
 		})
 		return
 	}
 
 	event := data.LogEntry{
-		Name: req.Name,
-		Data: req.Data,
+		Method:       req.Method,
+		Path:         req.Path,
+		RemoteAddr:   req.RemoteAddr,
+		ResponseTime: req.ResponseTime,
+		StartTime:    req.StartTime,
+		StatusCode:   req.StatusCode,
 	}
 
 	err := app.Models.LogEntry.Insert(event)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": true,
+			"error":   true,
 			"message": err,
 		})
 		return
 	}
 
 	ctx.JSON(http.StatusAccepted, gin.H{
-		"error": false,
+		"error":   false,
 		"message": "logs saved",
 	})
 }
