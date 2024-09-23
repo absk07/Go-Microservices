16682e940bffff4fca87b6105b85581d332909fc
absk <noobmaster26128@gmail.com>
2024-09-24 00:36:38 +0530

    added exchange, consumer & queue functionality

---
+++ listener-service/events/consumer.go
@@ -0,0 +1,139 @@
+package events
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return Consumer{}, err
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(channel)
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (consumer *Consumer) Listen(topics []string) error {
+	ch, err := consumer.conn.Channel()
+	if err != nil {
+		return nil
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return nil
+	}
+
+	for _, topic := range topics {
+		err := ch.QueueBind(
+			q.Name,
+			topic,
+			"logs",
+			false,
+			nil,
+		)
+		if err != nil {
+			return nil
+		}
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil
+	}
+
+	forever := make(chan bool)
+	go func() {
+		for msg := range msgs {
+			var payload Payload
+			_ = json.Unmarshal(msg.Body, &payload)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	log.Printf("Waiting for messages [Exchange, Queue] [logs, %s]\n", q.Name)
+	<-forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	case "auth":
+		// authenticate
+	default:
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func logEvent(payload Payload) error {
+	jsonData, _ := json.Marshal(payload)
+
+	request, err := http.NewRequest("POST", "http://logger-service:7070/log", bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		// log.Println("http req err", err)
+		return err
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+
+	if err != nil {
+		// log.Println("http res err", err)
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		return err
+	}
+
+	return nil
+}
--- listener-service/main.go
+++ listener-service/main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"log"
 
+	"github.com/listener-service/events"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	// connect to rabbitmq
 	conn, err := connect()
 	if err != nil {
 		log.Panicf("Error connecting to rabbitmq: %s", err)
 	}
 	defer conn.Close()
+
+	// start listening for mesages
+	log.Println("Listenibg and consuming for RabbitMQ messages")
+
+	// create consumer
+	consumer, err := events.NewConsumer(conn)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func connect() (*amqp.Connection, error) {
