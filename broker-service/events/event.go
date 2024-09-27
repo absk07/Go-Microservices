211152d82b10186117bd25431f6217e6203d03a7
absk <noobmaster26128@gmail.com>
2024-09-28 00:50:11 +0530

    removed unused code

--- broker-service/events/event.go
+++ broker-service/events/event.go
@@ -21,14 +21,3 @@ func declareExchange(ch *amqp.Channel) error {
 		nil,     // arguments
 	)
 }
-
-func declareRandomQueue(ch *amqp.Channel) (amqp.Queue, error) {
-	return ch.QueueDeclare(
-		"",    // name
-		false, // durable
-		false, // delete when unused
-		true,  // exclusive
-		false, // no-wait
-		nil,   // arguments
-	)
-}
